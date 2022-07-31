3848d8370269c7eff76021a59ab0e8421a311b15
itsJRillo <jmorillolabour@gmail.com>
2022-07-31 18:09:10 +0200

    Error Fixed

--- guitarla/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '537cb78610c14197221f09cd9af585f6'),
-    },
-  },
-});
